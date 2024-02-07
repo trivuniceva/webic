ba7eedb6b7f7745f559bb3a09a5417bb52c7a8f1
trivuniceva <trivunic99@gmail.com>
2024-02-07 01:32:58 +0100

    sve

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <NavbarComp />
+    <router-view />
+  </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavbarComp from "@/components/NavbarComp.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {NavbarComp},
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,39 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import axios from 'axios';
+import { createRouter, createWebHistory } from 'vue-router';
+import store from "@/store";
 
-createApp(App).mount('#app')
+import LoginComponent from "@/components/LoginComponent.vue";
+import HomepageComponent from "@/components/HomepageComponent.vue";
+import SearchUsersComponent from "@/components/SearchUsersComponent.vue";
+import SignUpView from "@/components/SignUpView.vue";
+import userProfileViewComponent from "@/components/userProfileViewComponent.vue";
+import homeLoggedComp from "@/components/loggedUserComp/homeLoggedComp.vue";
+import messagesLogged from "@/components/loggedUserComp/messagesLoggedComp.vue";
+import searchLoggedComp from "@/components/loggedUserComp/searchLoggedComp.vue";
+import editProfileComp from "@/components/loggedUserComp/editProfileComp.vue";
+import changePasswordComp from "@/components/loggedUserComp/changePasswordComp.vue";
+
+const routes = [
+    { path: '/', component: HomepageComponent },
+    { path: '/login', component: LoginComponent },
+    { path: '/signup', component: SignUpView },
+    { path: '/searchUsers', component: SearchUsersComponent },
+    { path: '/userProfileView', component: userProfileViewComponent },
+    { path: '/homeLogged', component:  homeLoggedComp},
+    { path: '/searchLogged', component: searchLoggedComp},
+    { path: '/messagesLogged', component:  messagesLogged},
+    { path: '/editProfile', component: editProfileComp },
+    { path: '/changePassword', component: changePasswordComp},
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+const app = createApp(App);
+app.config.globalProperties.$http = axios;
+app.use(store);
+app.use(router).mount('#app');
